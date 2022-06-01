b84824c37c560ccca419c69eb4bd35f2d0847778
Igor Pustovoy <igorpustovoy1978@gmail.com>
2022-06-01 10:23:35 +0200

    1

---
+++ lab09/zad1.py
@@ -0,0 +1,69 @@
+import matplotlib.pyplot as plt
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.probability import FreqDist
+
+nltk.download([
+    "names",
+    "stopwords",
+    "state_union",
+    "averaged_perceptron_tagger",
+    "vader_lexicon",
+    "punkt",
+    "wordnet",
+    "omw-1.4"
+])
+
+with open('school_shooting.txt') as f:
+    article = f.read()
+
+# print(article)
+
+# b)
+tokens = nltk.word_tokenize(article)
+# Changing all upper case letters to lower case ones
+tokens = [w.lower() for w in tokens]
+# print(tokens)
+# print(len(tokens))
+# Words: 900
+
+# c)
+stop_words = set(stopwords.words("english"))
+# print(stop_words)
+
+filtered_tokens = []
+for word in tokens:
+    if word not in stop_words:
+        filtered_tokens.append(word)
+
+# print(len(filtered_tokens))
+# Words: 587
+# d)
+signs = {'``', "''", ".", ",", "-", "$", "?", "!", "the"}
+stop_words = stop_words.union(signs)
+
+filtered_tokens = []
+for word in tokens:
+    if word not in stop_words:
+        filtered_tokens.append(word)
+
+# print(filtered_tokens)
+# print(len(filtered_tokens))
+# Words: 462
+
+# e)
+lem = WordNetLemmatizer()
+
+lematized_tokens = []
+for word in filtered_tokens:
+    lematized_tokens.append(lem.lemmatize(word, "v"))
+
+print(lematized_tokens)
+# print(len(lematized_tokens))
+# Words: 462
+
+# f)
+fdist = FreqDist(lematized_tokens)
+fdist.plot(10, cumulative=False)
+plt.show()
